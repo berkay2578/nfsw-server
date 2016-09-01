405c82cac08753e36d18c90dc07a952aef4c6cf2
berkay2578 <berkaytgy@gmail.com>
2016-09-01 15:16:41 +0300

    Add missing calculation

--- OfflineServer/Servers/Xmpp/BasicXmppServer.cs
+++ OfflineServer/Servers/Xmpp/BasicXmppServer.cs
@@ -90,6 +90,77 @@ namespace OfflineServer.Servers.Xmpp
             }
         }
 
+        // Only copied my calculation from https://raw.githubusercontent.com/nilzao/soapbox-race/master/src/main/java/br/com/soapboxrace/engine/Router.java
+        // Will eventually re-write it.
+        // For the unaware: THIS CODE IS NOT READY FOR PRODUCTION.
+        protected static Object[] __allmul(long multiplier, long multiplicand)
+        {
+            long hiMultiplier = (multiplier >> 32) & 0xffffffffL;
+            long loMultiplier = ((multiplier << 32) >> 32) & 0xffffffffL;
+
+            long hiMultiplicand = (multiplicand >> 32) & 0xffffffffL;
+            long loMultiplicand = ((multiplicand << 32) >> 32) & 0xffffffffL;
+
+            long multiplied, loMultiplied, hiMultiplied, oldMultiplied;
+            if (hiMultiplicand == 0 && hiMultiplier == 0)
+                multiplied = loMultiplier * loMultiplicand;
+            else
+                multiplied = multiplier * multiplicand;
+            oldMultiplied = multiplied;
+            hiMultiplied = (multiplied >> 32) & 0xffffffffL;
+            loMultiplied = ((multiplied << 32) >> 32) & 0xffffffffL;
+            do
+            { // modulo wrapper on UInt32
+                if (loMultiplied < 0L || loMultiplied > 4294967295L)
+                    loMultiplied %= 4294967296L;
+                if (hiMultiplied < 0L || hiMultiplied > 4294967295L)
+                    hiMultiplied %= 4294967296L;
+            } while ((loMultiplied < 0L || hiMultiplied < 0L)
+                    && (loMultiplied > 4294967295L || hiMultiplied > 4294967295L));
+            multiplied = hiMultiplied << 32 | loMultiplied;
+            return new Object[] { multiplied, oldMultiplied == loMultiplied };
+        }
+
+        public static Int64 calculateHash(char[] jid, char[] response)
+        {
+            long multiplier = 4294967295L & 0xffffffffL;
+            bool cFlag = true;
+            foreach(char c in jid)
+            {
+                Object[] bHash = __allmul(multiplier, 33L & 0xffffffffL);
+                long jidHash = (long)bHash[0];
+                long hiJidHash = (jidHash >> 32) & 0xffffffffL;
+                long loJidHash = ((jidHash << 32) >> 32) & 0xffffffffL;
+
+                long hiCdq = hiJidHash;
+                long loCdq = Convert.ToInt64(c) & 0xffffffffL;
+
+                long hiMultiplier = (((hiJidHash >> 32) + hiCdq) + (cFlag == true ? 1L : 0L)) & 0xffffffffL;
+                long loMultiplier = (((loJidHash << 32) >> 32) + loCdq) & 0xffffffffL;
+
+                multiplier = hiMultiplier << 32 | loMultiplier;
+                cFlag = (bool)bHash[1];
+            }
+
+            foreach(char c in response)
+            {
+                Object[] bHash = __allmul(multiplier, 33L & 0xffffffffL);
+                long jidHash = (long)bHash[0];
+                long hiJidHash = (jidHash >> 32) & 0xffffffffL;
+                long loJidHash = ((jidHash << 32) >> 32) & 0xffffffffL;
+
+                long hiCdq = hiJidHash;
+                long loCdq = Convert.ToInt64(c) & 0xffffffffL;
+
+                long hiMultiplier = (((hiJidHash >> 32) + hiCdq) + (cFlag == true ? 1L : 0L)) & 0xffffffffL;
+                long loMultiplier = (((loJidHash << 32) >> 32) + loCdq) & 0xffffffffL;
+
+                multiplier = hiMultiplier << 32 | loMultiplier;
+                cFlag = (bool)bHash[1];
+            }
+            return multiplier;
+        }
+
         public override void shutdown()
         {
             if (cts != null) cts.Cancel();
