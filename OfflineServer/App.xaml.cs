bd5380f1a1cd01a41ce305b7d67d1e79c445b5fe
berkay2578 <berkaytgy@gmail.com>
2016-09-04 23:02:05 +0300

    Simplify delegate call

--- OfflineServer/App.xaml.cs
+++ OfflineServer/App.xaml.cs
@@ -12,9 +12,7 @@ namespace OfflineServer
 
         protected void RaisePropertyChangedEvent(string propertyName)
         {
-            var handler = PropertyChanged;
-            if (handler != null)
-                handler(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
     [AttributeUsage(AttributeTargets.Field)]
