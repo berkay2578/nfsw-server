cd836b8375135308c103e96a8b1e2791cb18972a
berkay2578 <berkaytgy@gmail.com>
2017-02-02 12:32:12 +0300

    Speculative fix for the crash if the registry key doesn't exist

--- OfflineServer/Data/DataAccess.cs
+++ OfflineServer/Data/DataAccess.cs
@@ -2,11 +2,15 @@
 using OfflineServer.Data.Settings;
 using System;
 using System.IO;
+using System.Reflection;
+using System.Security;
+using System.Windows;
 
 namespace OfflineServer.Data
 {
     public class DataAccess
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         public AppSettings appSettings { get; set; }
 
         public void executablesAddNew()
@@ -35,31 +39,53 @@ namespace OfflineServer.Data
             executablesAddNew();
 
             #region Registry Check @App-Start
-            RegistryKey regBaseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
-            RegistryKey nfswKey = regBaseKey.OpenSubKey(@"Software\Electronic Arts\Need For Speed World\", false);
-
-            if (nfswKey == null)
+            log.Info("Trying to read registy.");
+            RegistryKey regBaseKey = null, nfswKey = null;
+            try
             {
-                regBaseKey.Dispose();
-                nfswKey.Dispose();
-                regBaseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                regBaseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
                 nfswKey = regBaseKey.OpenSubKey(@"Software\Electronic Arts\Need For Speed World\", false);
-            }
 
-            if (nfswKey != null)
-            {
-                Object installDir = nfswKey.GetValue("GameInstallDir");
-                if (installDir != null)
+                if (nfswKey == null)
                 {
-                    String nfsw_exe = Path.Combine(installDir.ToString(), "Data", "nfsw.exe");
-                    if (!appSettings.NFSWorldExecutables.Contains(nfsw_exe))
+                    log.Info("Key doesn't exist in Registry32, checking Registry64.");
+                    regBaseKey.Dispose();
+
+                    regBaseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                    nfswKey = regBaseKey.OpenSubKey(@"Software\Electronic Arts\Need For Speed World\", false);
+                    log.InfoFormat("Registry64 key status: {0}", nfswKey);
+                }
+
+                if (nfswKey != null)
+                {
+                    log.Info("Key found.");
+                    Object installDir = nfswKey.GetValue("GameInstallDir");
+                    if (installDir != null)
                     {
-                        appSettings.NFSWorldExecutables.Add(nfsw_exe);
+                        log.InfoFormat("GameInstallDir found: {0}", installDir.ToString());
+                        String nfsw_exe = Path.Combine(installDir.ToString(), "Data", "nfsw.exe");
+                        if (!appSettings.NFSWorldExecutables.Contains(nfsw_exe))
+                        {
+                            appSettings.NFSWorldExecutables.Add(nfsw_exe);
+                        }
                     }
                 }
             }
-            regBaseKey.Dispose();
-            nfswKey.Dispose();
+            catch (SecurityException sex) // ( ͡° ͜ʖ ͡°)
+            {
+                log.Error("A security exception occured while working on registry.", sex);
+                MessageBox.Show("This user doesn't have the permissions required to read registry.\r\n" +
+                    "OfflineServer won't be able to locate nfsw.exe automatically.",
+                    "A security exception occured!",
+                    MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.ServiceNotification);
+            }
+            finally
+            {
+                if (regBaseKey != null)
+                    regBaseKey.Dispose();
+                if (nfswKey != null)
+                    nfswKey.Dispose();
+            }
             #endregion
 
             appSettings.saveInstance();
@@ -67,18 +93,22 @@ namespace OfflineServer.Data
 
         public DataAccess()
         {
+            log.Info("Setting up DataAccess.");
             #region AppSettings
             if (DataEx.xml_SettingsExists)
             {
+                log.Info("Reading from local AppSettings.");
                 appSettings = DataEx.getInstanceFromXml<AppSettings>();
                 if (appSettings == default(AppSettings))
                 {
+                    log.Warn("Instance is null! Creating new.");
                     appSettings = new AppSettings();
                     appSettings.saveInstance();
                 }
             }
             else
             {
+                log.Warn("AppSettings doesn't exist! Creating new.");
                 appSettings = new AppSettings();
                 appSettings.saveInstance();
             }
